5c9beea8e4c6c4f6d74bcda7fbb85d6b229aa8b4
arunlalto <arunlalto@gmail.com>
2024-11-22 15:24:36 +0530

    admin/add-items component cpmpleted then setup server and API call

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,11 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 
-export const connectDB = async ()=>{
-    await mongoose.connect('mongodb+srv://arunlalto:8606838208@cluster0.bxvas.mongodb.net/Tomato-food-delivery-app').then(()=>{console.log("DB Connected");
-    })
-}
+export const connectDB = async () => {
+  await mongoose
+    .connect(
+      "mongodb+srv://arunlalto:8606838208@cluster0.bxvas.mongodb.net/Tomato-food-delivery-app"
+    )
+    .then(() => {
+      console.log("DB Connected");
+    });
+};
