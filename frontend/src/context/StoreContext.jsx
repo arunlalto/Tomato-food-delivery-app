a55358ee6269f6ccd68d86bc1e3326edc16fc3d0
ARUNLAL TO <88615868+arunlalto@users.noreply.github.com>
2024-12-10 11:12:28 +0530

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -6,7 +6,7 @@ const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
   const [token, setToken]=useState("");
   const [food_list,setFood_list]=useState([]);
-  const url="http://localhost:4000"
+  const url="https://tomato-food-delivery-backend-ghti.onrender.com"
   const addToCart = async(itemId) => {
     if (!cartItems[itemId]) {
       setCartItems((prev) => ({ ...prev, [itemId]: 1 }));
