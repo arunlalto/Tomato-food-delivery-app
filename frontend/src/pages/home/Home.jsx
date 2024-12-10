1d38e3e053e51acee9a15827e9e55f4288a5595e
ARUNLAL TO <88615868+arunlalto@users.noreply.github.com>
2024-12-10 11:33:26 +0530

    Update Home.jsx

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import './Home.css'
 import Header from '../../components/header/Header'
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
-import FoodDisplay from '../../components/foodDisplay/foodDisplay'
+import FoodDisplay from '../../components/foodDisplay/FoodDisplay'
 import AppDownload from '../../components/appDownload/AppDownload'
 
 const Home = () => {
